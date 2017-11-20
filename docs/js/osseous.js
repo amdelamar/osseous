3e8397ab15dd078455b5695592b1f52f2a2aa321
amdelamar <amdelamar@protonmail.com>
2017-11-20 14:34:23 -0500

    Add navbar and sidebar example

--- docs/js/osseous.js
+++ docs/js/osseous.js
@@ -4,26 +4,12 @@
  * MIT License
  */
 function openSidebar() {
-    document.getElementById("sidebar").style.display = "block";
+    document.getElementById("sidebar").style.display = "inline-block";
 }
 function closeSidebar() {
     document.getElementById("sidebar").style.display = "none";
 }
-window.onclick = function(event) {
-	// When the user clicks anywhere outside of the modal, close it
-	var modal = document.getElementById('share-popup');
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-function openPopup(name) {
-	document.getElementById(name).style.display='block';
-}
-function closePopup(name) {
-	document.getElementById(name).style.display='none';
-}
-// When the user clicks on the button, scroll to the top of the document
 function gotoTop() {
   document.body.scrollTop = 0; // For Chrome, Safari and Opera
   document.documentElement.scrollTop = 0; // For IE and Firefox
-} 
+}
